544c50c7d14236fa732cb5e4f73ac9d7069dc1c2
Cobalt Chang <cobalt.chang@gmail.com>
2019-05-04 09:11:25 -0400

    1. Refactory: single responsibility
    	modified:   bowling_game_kata.py

--- bowling_game_kata.py
+++ bowling_game_kata.py
@@ -1,10 +1,16 @@
 class Game:
 
     def __init__(self):
-        self._score = 0
+        self._scores = [0] * 21
+        self._index = 0
 
     def roll(self, pins):
-        self._score += pins
+        self._scores[self._index] = pins
+        self._index += 1
 
     def score(self):
-        return self._score
+        score = 0
+        for i in range(len(self._scores)):
+            score += self._scores[i]
+
+        return score
