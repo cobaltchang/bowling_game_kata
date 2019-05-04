206cb435366c57bfff015695ab656016f4d834d2
Cobalt Chang <cobalt.chang@gmail.com>
2019-05-04 09:15:42 -0400

    1. Refactory: Pythonic
    	modified:   bowling_game_kata.py

--- bowling_game_kata.py
+++ bowling_game_kata.py
@@ -1,12 +1,10 @@
 class Game:
 
     def __init__(self):
-        self._scores = [0] * 21
-        self._index = 0
+        self._scores = []
 
     def roll(self, pins):
-        self._scores[self._index] = pins
-        self._index += 1
+        self._scores.append(pins)
 
     def score(self):
         score = 0
