a8cc83654056aa4174e4cfb56ce736170ec13054
Cobalt Chang <cobalt.chang@gmail.com>
2019-05-04 04:22:39 -0400

    1. Test case: roll all zeros.
    	modified:   bowling_game_kata.py
    	modified:   tests/test_bowling_game_kata.py

--- bowling_game_kata.py
+++ bowling_game_kata.py
@@ -1,3 +1,10 @@
-
 class Game:
-    pass
+
+    def __init__(self):
+        self._score = 0
+
+    def roll(self, pins):
+        self._score += pins
+
+    def score(self):
+        return self._score
--- tests/test_bowling_game_kata.py
+++ tests/test_bowling_game_kata.py
@@ -10,5 +10,9 @@ from bowling_game_kata import Game  # noqa: E402
 
 class GameTestCase(unittest.TestCase):
 
-    def test_(self):
-        pass
+    def test_roll_all_zeros(self):
+        game = Game()
+        for i in range(20):
+            game.roll(0)
+
+        self.assertEqual(game.score(), 0)
