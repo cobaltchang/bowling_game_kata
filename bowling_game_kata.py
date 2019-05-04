05a6442d369db09c09b9c5e321259a87ba0c5909
Cobalt Chang <cobalt.chang@gmail.com>
2019-05-04 10:51:58 -0400

    1. Refactory: Extract method.
    	modified:   bowling_game_kata.py

--- bowling_game_kata.py
+++ bowling_game_kata.py
@@ -11,13 +11,13 @@ class Game:
         roll_index = 0
         for frame in range(10):
             if self._is_strike(roll_index):
-                score += 10 + self._scores[roll_index + 1] + self._scores[roll_index + 2]
+                score += 10 + self._strike_bonus(roll_index)
                 roll_index += 1
             elif self._is_spare(roll_index):
-                score += 10 + self._scores[roll_index + 2]
+                score += 10 + self._spare_bonus(roll_index)
                 roll_index += 2
             else:
-                score += self._scores[roll_index] + self._scores[roll_index + 1]
+                score += self._sum_of_rolls_in_frame(roll_index)
                 roll_index += 2
 
         return score
@@ -27,3 +27,12 @@ class Game:
 
     def _is_spare(self, roll_index):
         return (self._scores[roll_index] + self._scores[roll_index + 1]) == 10
+
+    def _sum_of_rolls_in_frame(self, roll_index):
+        return self._scores[roll_index] + self._scores[roll_index + 1]
+
+    def _strike_bonus(self, roll_index):
+        return self._scores[roll_index + 1] + self._scores[roll_index + 2]
+
+    def _spare_bonus(self, roll_index):
+        return self._scores[roll_index + 2]
